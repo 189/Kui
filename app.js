ff5709ec612df1183edcbe1713bf85665dc33293
Shangwen Wang <kv.wang@gmail.com>
2016-07-25 16:32:43 +0800

    单选和 switch

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-/* basic */
-import './css/reset.css';
-import './css/query.css';
-import './css/font.css';
-import './css/basic.css';
-import './css/1px.css';
-import './css/layout.css';
+/* Load basic css module */
+import './js/basic';
 
-/*  */
+
+/* Load form css module  */
+import './js/form.js';
---
+++ js/basic.js
@@ -0,0 +1,7 @@
+/* basic */
+import '../css/reset.css';
+import '../css/query.css';
+import '../css/font.css';
+import '../css/basic.css';
+import '../css/1px.css';
+import '../css/layout.css';
